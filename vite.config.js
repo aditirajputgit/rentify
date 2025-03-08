c86c6166566cbe8fc84850f8e2cc621cd814919f
Aditi Rajput <aditirajput200017@gmail.com>
2025-03-08 22:39:28 +0530

    gg

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/rentify/', // Corrected base configuration
+  build: {
+    outDir: 'dist', // Output directory for the build
+  },
+});
